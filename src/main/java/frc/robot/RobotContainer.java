94be2e5f0fb365aa7f81e43c7af0bf257c359443
Michael Dahlberg <dahlberg.michael.r@gmail.com>
2026-03-14 15:11:10 -0400

    Tweaks

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -102,9 +102,9 @@ public class RobotContainer {
         // m_algaeSubsystem.setDefaultCommand(m_algaeSubsystem.idleCommand());
         // Register named commands for PathPlanner
         NamedCommands.registerCommand("StartIntake",  m_intake.runIntakeCommand());
-        NamedCommands.registerCommand("StopIntake",   m_intake.runExtakeCommand());
-        NamedCommands.registerCommand("SlapUp",       m_intake.runSlapUpCommand().withTimeout(0.5)); // TODO add stop logic
-        NamedCommands.registerCommand("SlapDown",     m_intake.runSlapDownCommand().withTimeout(0.5)); // TODO add stop logic
+        NamedCommands.registerCommand("StopIntake",   m_intake.runOnce(() -> {});
+        NamedCommands.registerCommand("SlapArmUp",    m_intake.runSlapUpCommand().withTimeout(0.5)); // TODO add stop logic
+        NamedCommands.registerCommand("SlapArmDown",  m_intake.runSlapDownCommand().withTimeout(0.5)); // TODO add stop logic
         NamedCommands.registerCommand("StartShoot",   m_shooter.runShooterCommand());
         NamedCommands.registerCommand("StopShoot",    m_shooter.runOnce(() -> {}));
 
