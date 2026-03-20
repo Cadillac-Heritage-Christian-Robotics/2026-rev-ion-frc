52747c47c240d390047899544e19ef45358fb453
mdahlberg <mdahlber@gmail.com>
2026-03-20 14:49:07 -0400

    Predefined starting positions (#25)
    Co-authored-by: Michael Dahlberg <dahlberg.michael.r@gmail.com>

--- src/main/java/frc/robot/AutonUtils.java
+++ src/main/java/frc/robot/AutonUtils.java
@@ -11,6 +11,18 @@ import frc.robot.Constants.TargetPositions;
 public final class AutonUtils {
     public static final Map<String, Pose2d> AUTON_START_POSES = Map.ofEntries(
         // Blue North
+        Map.entry("BlueNorth",   new Pose2d(3.454, 7.622, Rotation2d.fromDegrees(0))),
+        Map.entry("BlueCenter",  new Pose2d(3.454, 4.041, Rotation2d.fromDegrees(0))),
+        Map.entry("BlueSouth",   new Pose2d(3.454, 0.460, Rotation2d.fromDegrees(0))),
+
+        // Red North
+        Map.entry("RedNorth",    new Pose2d(13.080, 7.622, Rotation2d.fromDegrees(180.0))),
+        Map.entry("RedCenter",   new Pose2d(13.080, 4.041, Rotation2d.fromDegrees(180.0))),
+        Map.entry("RedSouth",    new Pose2d(13.080, 0.460, Rotation2d.fromDegrees(180.0)))
+    );
+
+    public static final Map<String, Pose2d> AUTON_TARGET_POSES = Map.ofEntries(
+        // Blue North
         Map.entry("BlueNorthAlpha",   new Pose2d(2.938, 4.808, Rotation2d.fromDegrees(83.478))),
         Map.entry("BlueNorthBravo",   new Pose2d(2.938, 4.808, Rotation2d.fromDegrees(119.775))),
         Map.entry("BlueNorthDefault", new Pose2d(2.938, 4.808, Rotation2d.fromDegrees(83.478))),
@@ -37,8 +49,18 @@ public final class AutonUtils {
         return TargetPositions.BLUE_SHOOT;
     }
 
-    public static Pose2d getStartingPose(String strategy) throws IllegalArgumentException {
-        Pose2d pose = AUTON_START_POSES.get(strategy);
+    public static Pose2d getActualStartingPos(String strategy) throws IllegalArgumentException {
+        String position = strategy.replace("Alpha", "").replace("Bravo", "").replace("Default", "");
+        Pose2d pose = AUTON_START_POSES.get(position);
+        if (pose == null) {
+            throw new IllegalArgumentException("No starting pose found for position: '" + position + "' from strategy: " + strategy);
+        }
+
+        return pose;
+    }
+
+    public static Pose2d getDesiredPose(String strategy) throws IllegalArgumentException {
+        Pose2d pose = AUTON_TARGET_POSES.get(strategy);
         if (pose == null) {
             throw new IllegalArgumentException("No starting pose found for strategy: " + strategy);
         }
