2fa4f2831984156c942bae6a3997a0c6e490afb1
Michael Dahlberg <dahlberg.michael.r@gmail.com>
2026-04-10 17:37:34 -0400

    Added new path to test actions in pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -128,6 +128,7 @@ public class RobotContainer {
         m_autoStrategy.setDefaultOption("Default", "Default");
         m_autoStrategy.addOption("Alpha", "Alpha");
         m_autoStrategy.addOption("Bravo", "Bravo");
+        m_autoStrategy.addOption("Default With Action", "DefaultAction");
 
         // Push it to SmartDashboard so drive team can see it
         SmartDashboard.putData("Drive Backward Strategy", m_useDeferred);
